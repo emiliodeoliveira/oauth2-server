93b82f18943d8d6a72e37050305171d241e55e6d
Emilio de Oliveira <emilio.oliveira@safelight.info>
2024-04-25 15:11:42 -0300

    #3 - Fixes the wrong identation

--- models.py
+++ models.py
@@ -31,35 +31,38 @@ class User(UserMixin, db.Model):
     def __repr__(self):
         return '<User %r>' % self.id
 
-    @dataclass
-    class OAuth2Client(db.Model, OAuth2ClientMixin):
-        __tablename__ = 'oauth2_client'
 
-        id = db.Column(db.Integer, primary_key=True)
-        user_id = db.Column(
-            db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'))
-        user = db.relationship('User')
+@dataclass
+class OAuth2Client(db.Model, OAuth2ClientMixin):
+    __tablename__ = 'oauth2_client'
 
-    @dataclass
-    class OAuth2AuthorizationCode(db.Model, OAuth2AuthorizationCodeMixin):
-        __tablename__ = 'oauth2_code'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(
+        db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'))
+    user = db.relationship('User')
 
-        id = db.Column(db.Integer, primary_key=True)
-        user_id = db.Column(
-            db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'))
-        user = db.relationship('User')
 
-    @dataclass
-    class OAuth2Token(db.Model, OAuth2TokenMixin):
-        __tablename__ = 'oauth2_token'
+@dataclass
+class OAuth2AuthorizationCode(db.Model, OAuth2AuthorizationCodeMixin):
+    __tablename__ = 'oauth2_code'
 
-        id = db.Column(db.Integer, primary_key=True)
-        user_id = db.Column(
-            db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'))
-        user = db.relationship('User')
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(
+        db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'))
+    user = db.relationship('User')
 
-        def is_refresh_token_active(self):
-            if self.revoked:
-                return False
-            expires_at = self.issued_at + self.expires_in * 2
-            return expires_at >= time.time()
+
+@dataclass
+class OAuth2Token(db.Model, OAuth2TokenMixin):
+    __tablename__ = 'oauth2_token'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(
+        db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'))
+    user = db.relationship('User')
+
+    def is_refresh_token_active(self):
+        if self.revoked:
+            return False
+        expires_at = self.issued_at + self.expires_in * 2
+        return expires_at >= time.time()

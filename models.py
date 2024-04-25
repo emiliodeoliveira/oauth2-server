f2f93896de6902aa802ad33b8f5837b79fd74e4e
Emilio de Oliveira <emilio.oliveira@safelight.info>
2024-04-25 15:06:34 -0300

    #3 - Model refactor and includes OAuth2 classes

--- models.py
+++ models.py
@@ -1,6 +1,14 @@
-from datetime import datetime
+import time
 from dataclasses import dataclass
+from datetime import datetime
+
+from authlib.integrations.sqla_oauth2 import (
+    OAuth2ClientMixin,
+    OAuth2AuthorizationCodeMixin,
+    OAuth2TokenMixin,
+)
 from flask_login import UserMixin
+
 from . import db
 
 
@@ -22,3 +30,36 @@ class User(UserMixin, db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.id
+
+    @dataclass
+    class OAuth2Client(db.Model, OAuth2ClientMixin):
+        __tablename__ = 'oauth2_client'
+
+        id = db.Column(db.Integer, primary_key=True)
+        user_id = db.Column(
+            db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'))
+        user = db.relationship('User')
+
+    @dataclass
+    class OAuth2AuthorizationCode(db.Model, OAuth2AuthorizationCodeMixin):
+        __tablename__ = 'oauth2_code'
+
+        id = db.Column(db.Integer, primary_key=True)
+        user_id = db.Column(
+            db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'))
+        user = db.relationship('User')
+
+    @dataclass
+    class OAuth2Token(db.Model, OAuth2TokenMixin):
+        __tablename__ = 'oauth2_token'
+
+        id = db.Column(db.Integer, primary_key=True)
+        user_id = db.Column(
+            db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'))
+        user = db.relationship('User')
+
+        def is_refresh_token_active(self):
+            if self.revoked:
+                return False
+            expires_at = self.issued_at + self.expires_in * 2
+            return expires_at >= time.time()
